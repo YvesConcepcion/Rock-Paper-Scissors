f23e58ec611fc74da7c8d5fa2d0a36e5047fd34a
YvesConcepcion <118972229+YvesConcepcion@users.noreply.github.com>
2023-03-14 18:25:15 +0800

    Fix Flex items for Mobile

--- script.js
+++ script.js
@@ -1,58 +1,81 @@
-"use strict";
+'use strict';
 
-let playerScore = 0;
-let compScore = 0;
+//selecting elements
+const btnRock = document.querySelector('.btn--rock');
+const btnPaper = document.querySelector('.btn--paper');
+const btnScissors = document.querySelector('.btn--scissors');
+const score0el = document.getElementById('.playerscore');
+const score1el = document.getElementById('.botscore');
+
+let playerScore, compScore, playing, playerSelection, computerSelection;
+
+//starting conditions
+const init = function () {
+  playerScore = 0;
+  compScore = 0;
+  playing = true;
+  playerSelection = '';
+  computerSelection = '';
+};
+
+init();
+
+//playRound
+const playRound = function (playerSelection, computerSelection) {
+  if (
+    (computerSelection === 'Rock' && playerSelection === 'Paper') ||
+    (computerSelection === 'Scissors' && playerSelection === 'Rock') ||
+    (computerSelection === 'Paper' && playerSelection === 'Scissors')
+  ) {
+    let result = 'win';
+    playerScore += 1;
+    console.log(`You ${result}! ${playerSelection} beats ${computerSelection}`);
+    console.log(playerScore);
+    return playerScore;
+  } else if (
+    (computerSelection === 'Paper' && playerSelection === 'Rock') ||
+    (computerSelection === 'Rock' && playerSelection === 'Scissors') ||
+    (computerSelection === 'Scissors' && playerSelection === 'Paper')
+  ) {
+    let result = 'lose';
+    compScore += 1;
+    console.log(`You ${result}! ${playerSelection} beats ${computerSelection}`);
+    console.log(compScore);
+    return compScore;
+  } else {
+    let result = 'tie';
+    console.log("It's a tie!");
+    return result;
+  }
+};
+
 const getComputerChoice = function () {
-  const computerArray = ["rock", "paper", "scissors"];
+  const computerArray = ['Rock', 'Paper', 'Scissors'];
   let random = Math.floor(Math.random() * computerArray.length);
   let computerSelection = computerArray[random];
   console.log(computerSelection);
   return computerSelection;
 };
 
-const playerSelection = function () {
-  let resp = prompt("What is your choice?");
-  let playerSelection = resp.toLowerCase();
-  console.log(playerSelection);
-  return playerSelection;
-};
+btnRock.addEventListener('click', function () {
+  playRound('Rock', getComputerChoice());
+});
 
-const playRound = function (playerSelection, computerSelection) {
-  if (
-    (computerSelection === "rock" && playerSelection === "paper") ||
-    (computerSelection === "scissors" && playerSelection === "rock") ||
-    (computerSelection === "paper" && playerSelection === "scissors")
-  ) {
-    let result = "win";
-    playerScore += 1;
-    console.log(`You ${result}! ${playerSelection} beats ${computerSelection}`);
-    console.log(playerScore);
-    return playerScore;
-  } else if (
-    (computerSelection === "paper" && playerSelection === "rock") ||
-    (computerSelection === "rock" && playerSelection === "scissors") ||
-    (computerSelection === "scissors" && playerSelection === "paper")
-  ) {
-    let result = "lose";
-    compScore += 1;
-    console.log(`You ${result}! ${playerSelection} beats ${computerSelection}`);
-    console.log(compScore);
-    return compScore;
-  } else {
-    let result = "tie";
-    console.log("It's a tie!");
-    return result;
+btnPaper.addEventListener('click', function () {
+  playRound('Paper', getComputerChoice());
+});
+
+btnScissors.addEventListener('click', function () {
+  playRound('Scissors', getComputerChoice());
+});
+
+const game = function () {
+  while (playerScore < 5 && compScore < 5) {
+    playRound(playerSelection(), getComputerChoice());
+    if (playerScore === 5) {
+      console.log('You win!');
+    } else if (compScore === 5) {
+      console.log('You lose!');
+    }
   }
 };
-
-// const game = function () {
-//   while (playerScore < 5 && compScore < 5) {
-//     playRound(playerSelection(), getComputerChoice());
-//     if (playerScore === 5) {
-//       console.log("You win!");
-//     } else if (compScore === 5) {
-//       console.log("You lose!");
-//     }
-//   }
-// };
-// game();
